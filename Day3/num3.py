730350fb6c059936a4ad517e509eb3c84a310c86
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-25 10:16:31 +0300

    Update num3.py

--- Day3/num3.py
+++ Day3/num3.py
@@ -1,16 +1,24 @@
+# import library
 import numpy as np
-#array
-x = input("Enter the the array ")
-
-if len(x) >5:
-
-    print("Enter more arrays")
-
-else:
-
-    arr = np.array([x])
-    print("Original array:")
-    print(arr)
-    # size of array
-    print("Size of the memory occupied by the said array:")
-    print("%d bytes" % (arr.size * arr.itemsize))
+# create list
+y = []
+# ask the size of array needed
+x = int(input("Size of array:\n"))
+# ask user to input the element they want to put
+for i in range(x):
+    #add data to the list
+    y.append(float(input("Element:\n")))
+# change the list to a numpy array
+y = np.array(y)
+print(y)
+# check the length of array
+if len(y) >5:
+    # get the size of the array
+    print("Size of the array: ",
+          y.size)
+    # get the memory size of one array element
+    print("Memory size of one array element in bytes: ",
+          y.itemsize)
+    # memory size of numpy array in bytes(all elements)
+    print("Memory size of numpy array in bytes:",
+          y.size * y.itemsize)
