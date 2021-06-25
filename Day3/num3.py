0fca969bfc81d8cb925cdeae87f40b845c9ce4c8
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-25 09:54:46 +0300

    Update num3.py

--- Day3/num3.py
+++ Day3/num3.py
@@ -2,9 +2,15 @@ import numpy as np
 #array
 x = input("Enter the the array ")
 
-arr = np.array([x])
-print("Original array:")
-print(arr)
-# size of array
-print("Size of the memory occupied by the said array:")
-print("%d bytes" % (arr.size * arr.itemsize))
+if len(x) >5:
+
+    print("Enter more arrays")
+
+else:
+
+    arr = np.array([x])
+    print("Original array:")
+    print(arr)
+    # size of array
+    print("Size of the memory occupied by the said array:")
+    print("%d bytes" % (arr.size * arr.itemsize))
