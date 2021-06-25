054a737448ba01e5f1b015dc2f4ab5800b43d7e8
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-25 09:50:39 +0300

    Update num3.py

--- Day3/num3.py
+++ Day3/num3.py
@@ -1,7 +1,8 @@
 import numpy as np
-
 #array
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+x = input("Enter the the array ")
+
+arr = np.array([x])
 print("Original array:")
 print(arr)
 # size of array
